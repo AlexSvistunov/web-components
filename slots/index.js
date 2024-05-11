d1694076affe90a9b270e3e111b1f988507fec55
AlexeySvistunov <xdayx53@gmail.com>
2024-05-11 14:06:45 +0300

    add slots example

--- slots/index.js
+++ slots/index.js
@@ -1,13 +1,25 @@
 customElements.define('user-card', class extends HTMLElement {
   connectedCallback() {
-    this.attachShadow({mode: 'open'});
-    this.shadowRoot.innerHTML = `
-      <div>Имя:
-        <slot name="username"></slot>
-      </div>
-      <div>Дата рождения:
-        <slot name="birthday"></slot>
-      </div>
-    `;
-  }
+
+      this.attachShadow({mode: 'open'});
+      this.shadowRoot.innerHTML = `
+        <div>Имя:
+          <slot name="username"></slot>
+        </div>
+        <div>Дата рождения:
+          <slot name="birthday"></slot>
+        </div>
+
+        <textarea></textarea>
+        <button>Submit</button>
+
+        <div class='children'>123321</div>
+      `;
+
+      console.log(this.shadowRoot)
+      
+      // const userCard = document.createElement('user-card');
+      // this.shadowRoot.querySelector('.children').appendChild(userCard);
+    }
+  
 });
