c23201db670f0888e79a998d39217ee78d7e0497
Alexey Svistunov <xdayx53@gmail.com>
2024-05-11 12:03:51 +0300

    slot is working

--- comment/index.js
+++ comment/index.js
@@ -1,6 +1,6 @@
 const input = document.querySelector(".input");
 const template = document.querySelector("template");
-console.log(template.content);
+
 
 customElements.define(
   "custom-comment",
@@ -8,45 +8,16 @@ customElements.define(
     constructor() {
       super();
 
-      this.attachShadow({ mode: "open" });
+      this.attachShadow({mode: 'open'})
+      this.shadowRoot.innerHTML = `
+        <slot name='layout'></slot>
+      `
+
+
     }
   }
 );
 
 
-document.querySelector(".button").addEventListener("click", () => {
-  if (input.value) {
-    const comment = document.createElement("custom-comment");
-    comment.innerHTML = `
 
-    
-    <div>
-      <div class="comment-head">
-        <span>Nickname</span>
-        <span>May 11, 2024</span>
-      </div>
 
-      <p>Some text</p>
-
-      <div class="comment-bottom">
-        <button class='reply'>Reply</button>
-        <button>Delete</button>
-      </div>
-
-      <div class='add-comment hidden'>
-        <textarea></textarea>
-        <button>Submit</button>
-
-      </div>
-
-      <div class='children'></div>
-
-    </div>
-    
-    `;
-    document.querySelector(".comments").appendChild(comment);
-  }
-});
-
-// <slot name='styles'></slot>
-// <slot name='layout'</slot>
