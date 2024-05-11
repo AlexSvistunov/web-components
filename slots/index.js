57647bae5fcd0d696e4e46649237f96337547ba2
AlexeySvistunov <xdayx53@gmail.com>
2024-05-11 14:25:50 +0300

    add layout and comment is slot

--- slots/index.js
+++ slots/index.js
@@ -1,25 +1,39 @@
-customElements.define('user-card', class extends HTMLElement {
-  connectedCallback() {
-
-      this.attachShadow({mode: 'open'});
-      this.shadowRoot.innerHTML = `
-        <div>Имя:
-          <slot name="username"></slot>
-        </div>
-        <div>Дата рождения:
-          <slot name="birthday"></slot>
-        </div>
-
-        <textarea></textarea>
-        <button>Submit</button>
-
-        <div class='children'>123321</div>
-      `;
-
-      console.log(this.shadowRoot)
-      
-      // const userCard = document.createElement('user-card');
-      // this.shadowRoot.querySelector('.children').appendChild(userCard);
+customElements.define(
+  "custom-comment",
+  class extends HTMLElement {
+    constructor() {
+      super();
     }
-  
-});
+
+    connectedCallback() {
+      this.attachShadow({ mode: "open" });
+      this.shadowRoot.innerHTML = `
+    <div class='comment-head'>
+      <span>Name</span>
+      <span>Date</span>
+    </div>
+
+    <p class='comment-text'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+
+    <div class='comment-buttons'>
+      <button>Reply</button>
+      <button>Delete</button>
+    </div>
+
+    <div class='comment-create'>
+      <textarea name="" id=""></textarea>
+      <button>Submit</button>
+    </div>
+
+    <div class="children">
+      
+    </div>
+    `;
+
+    this.shadowRoot.querySelector('textarea').value
+    }
+
+    // не нужно доходить до создания компонента
+
+  }
+);
