9dcbaecbb459e0072c07f8e7fbe0afcec4e1897e
Alexey Svistunov <xdayx53@gmail.com>
2024-05-11 12:00:36 +0300

    add more

--- comment/index.js
+++ comment/index.js
@@ -1,6 +1,6 @@
 const input = document.querySelector(".input");
-const template = document.querySelector('template')
-console.log(template)
+const template = document.querySelector("template");
+console.log(template.content);
 
 customElements.define(
   "custom-comment",
@@ -9,17 +9,41 @@ customElements.define(
       super();
 
       this.attachShadow({ mode: "open" });
-      
-
-
-   
     }
   }
 );
 
+
 document.querySelector(".button").addEventListener("click", () => {
   if (input.value) {
     const comment = document.createElement("custom-comment");
+    comment.innerHTML = `
+
+    
+    <div>
+      <div class="comment-head">
+        <span>Nickname</span>
+        <span>May 11, 2024</span>
+      </div>
+
+      <p>Some text</p>
+
+      <div class="comment-bottom">
+        <button class='reply'>Reply</button>
+        <button>Delete</button>
+      </div>
+
+      <div class='add-comment hidden'>
+        <textarea></textarea>
+        <button>Submit</button>
+
+      </div>
+
+      <div class='children'></div>
+
+    </div>
+    
+    `;
     document.querySelector(".comments").appendChild(comment);
   }
 });
